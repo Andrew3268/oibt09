8aed1521d24da7c615c87af53fbb558019d76e09
Your <scarlet3628@likelion.org>
2021-01-18 05:18:58 +0900

    admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  authenticate :user, lambda { |u| u.admin? } do
+    begin
+      mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+    rescue
+      redirect_to new_user_session_path
+    end
+  end
+
   resources :categories
   devise_for :users
   root 'promocodes#index'
