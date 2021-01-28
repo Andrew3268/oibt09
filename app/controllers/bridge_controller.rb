139b57ce0d5e5479be3b708cdc5d809fb28bda96
Your <scarlet3628@likelion.org>
2021-01-28 14:17:02 +0900

    added marketing

--- app/controllers/bridge_controller.rb
+++ app/controllers/bridge_controller.rb
@@ -1,7 +1,7 @@
 class BridgeController < ApplicationController
 
   def marketing
-    @promocodes = Promocode.all.order("created_at DESC")
+    @promocodes = Promocode.all.order("created_at DESC").limit(20)
   end
 
 end
